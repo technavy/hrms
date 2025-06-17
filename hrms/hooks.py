a6655706dbb1a043f1f9b666cfb87f9aa14e151b
Odilon Relucio <odilon@intelimina.com>
2025-06-17 20:25:30 +0800

    fix: update app logo to Technavy logo in hooks and subscription utils

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -12,8 +12,8 @@ app_home = "/app/overview"
 add_to_apps_screen = [
 	{
 		"name": "hrms",
-		"logo": "/assets/hrms/images/frappe-hr-logo.svg",
-		"title": "Frappe HR",
+		"logo": "/assets/hrms/images/technavy-logo.png",
+		"title": "PeopleCore",
 		"route": "/app/overview",
 		"has_permission": "hrms.hr.utils.check_app_permission",
 	}
